0bcb7bc08b09464efd7dab18684f4eea7160c2fd
Sugar0612 <287276293@qq.com>
2024-05-18 23:47:57 +0800

    feat: 添加 Database 和 json插件

---
+++ Assets/Scripts/Frame/DataBase/DatabaseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static kinderDatabase m_kiner;
+
+    public static kinderDatabase Kinder
+    {
+        get
+        {
+            if (m_kiner == null)
+            {
+                m_kiner = Resources.Load("DataBase/KinderDatabase") as kinderDatabase;
+            }
+            return m_kiner;
+        }
+    }
+}
---
+++ Assets/Scripts/Frame/DataBase/kinderDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UserTable
+{
+    public int id;
+    public string user;
+    public string password;
+    public int scores;
+}
+
+[CreateAssetMenu(fileName = "Kinder", menuName = "DataBase/KinderDatabase")]
+public class kinderDatabase : ScriptableObject
+{
+    public List<UserTable> users = new List<UserTable>();
+}
